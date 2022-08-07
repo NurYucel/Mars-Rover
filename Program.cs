cd4411259fb38f9c85e4e91dad80cf66719d3c9e
Nur Yücel <40251452+NurYucel@users.noreply.github.com>
2022-08-07 14:12:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MarsRover1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter upper-right coordinater of plateau. For example: " + "5 5 ");
+            string maxPoint = Console.ReadLine();
+
+            Console.WriteLine("Please enter rover's first position. For example: " + "1 2 N");
+            string firstRoverP = Console.ReadLine();
+
+            Console.WriteLine("Please indicate how the rover will explore the plateau. For axample: " + "LMLMLMLMM");
+            string firstRoverMoves = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Please enter rover's second position. For example: " + "3 3 E");
+            string secondRoverP = Console.ReadLine();
+
+            Console.WriteLine("Please indicate how the rover will explore the plateau. For axample: " + "MMRMMRMRRM");
+            string secondRoverMoves = Console.ReadLine().ToUpper();
+
+            int maxX, maxY;
+             Int32.TryParse(maxPoint.Split(" ")[0], out maxX);
+             Int32.TryParse(maxPoint.Split(" ")[1], out maxY);
+             
+
+            Rover rover = new Rover(firstRoverP);
+            rover.Move(firstRoverMoves);
+            if(rover.x<0|| rover.x >maxX|| rover.y < 0 || rover.y > maxY)
+            {
+                Console.WriteLine($"Positon can not be less than (0,0) and can not be greater than ({maxX},{maxY}). ");
+            }
+            else {
+                Console.WriteLine(rover.x + " " + rover.y + " " + rover.direction);
+            }
+            
+
+            Rover rover1 = new Rover(secondRoverP);
+            rover1.Move(secondRoverMoves);
+            if (rover1.x < 0 || rover1.x > maxX || rover1.y < 0 || rover1.y > maxY)
+            {
+                Console.WriteLine($"Positon can not be less than (0,0) and can not be greater than ({maxX},{maxY}). ");
+            }
+            else {
+                Console.WriteLine(rover1.x + " " + rover1.y + " " + rover1.direction);
+            }
+            
+        }
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover1
+{
+    public class Rover
+    {
+        public int x; //x cordinate
+        public int y; //y cordinate
+        public string direction; // variable of rover direction
+
+
+        public Rover(string location)
+        {
+            Int32.TryParse(location.Split(" ")[0], out x);
+            Int32.TryParse(location.Split(" ")[1], out y);
+            direction = location.Split(" ")[2].ToUpper();
+            
+        }
+        public void SpinLeft()
+        {
+            if (direction == "N")
+            {
+                direction = "W";
+            }
+            else if (direction == "E")
+            {
+                direction = "N";
+            }
+            else if (direction == "S")
+            {
+                direction = "E";
+            }
+            else if (direction == "W")
+            {
+                direction = "S";
+            }
+        }
+
+        public void SpinRight()
+        {
+
+            if (direction == "N")
+            {
+                direction = "E";
+            }
+            else if (direction == "E")
+            {
+                direction = "S";
+            }
+            else if (direction == "S")
+            {
+                direction = "W";
+            }
+            else if (direction == "W")
+            {
+                direction = "N";
+            }
+        }
+
+        public void StepFoward()
+        {
+
+            if (direction == "N") { y++; }
+            else if (direction == "E") { x++; }
+            else if (direction == "S") { y--; }
+            else if (direction == "W") { x--; }
+
+        }
+
+        public void Move(string roverCommand) // "LMLMLMLMM" OR "MMRMMRMRRM"
+        {
+            char[] moves = roverCommand.ToCharArray();        
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (moves[i] == 'L')
+                    { SpinLeft(); }
+                else if(moves[i] =='R')
+                    { SpinRight(); }
+                else if(moves[i] =='M')
+                    { StepFoward(); }
+            }
+        }
+    }
+}
